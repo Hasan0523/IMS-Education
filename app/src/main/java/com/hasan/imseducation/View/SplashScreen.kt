82e7865ccbec82cc5f8e40cff29ab14612b1f63c
PC680 <elshodxushvaqtov07@gmailc.om>
2024-01-29 13:02:33 +0500

    2

--- app/src/main/java/com/hasan/imseducation/View/SplashScreen.kt
+++ app/src/main/java/com/hasan/imseducation/View/SplashScreen.kt
@@ -1,10 +1,35 @@
 package com.hasan.imseducation.View
 
 import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
+import com.hasan.imseducation.R
+import com.hasan.imseducation.ui.theme.ButtonColor
 
 @Preview
 @Composable
@@ -15,5 +40,30 @@ fun PreviewSplash() {
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)){
+        Row (horizontalArrangement = Arrangement.Center, modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 25.dp)){
+            Text(text = "IMS Education", fontSize = 26.sp )
+        }
+        Row (horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Platformamizga xush kelibsiz", fontSize = 24.sp)
+        }
+        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp), painter = painterResource(R.drawable.ims_img), contentDescription ="Splash Img" )
+            Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = ButtonColor),modifier = Modifier.padding(10.dp)) {
+                Text(text = "Keyingisi")
+             
+         }
+
+        }
+
+
+    }
 
 }
