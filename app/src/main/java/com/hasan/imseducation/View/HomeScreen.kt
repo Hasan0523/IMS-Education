7fd5f7623996626e46655490bbd39b4056f16572
PC680 <elshodxushvaqtov07@gmailc.om>
2024-01-31 13:17:04 +0500

    3.2

--- app/src/main/java/com/hasan/imseducation/View/HomeScreen.kt
+++ app/src/main/java/com/hasan/imseducation/View/HomeScreen.kt
@@ -1,2 +1,20 @@
 package com.hasan.imseducation.View
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Preview
+@Composable
+fun HomeSplash() {
+    HomeScreen(navController = rememberNavController())
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column {
+
+    }
+}
